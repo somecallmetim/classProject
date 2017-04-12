15f90a6b9aaf0e5d13dfeb6754fec0e326a43ec1
mayara <mayarapb.cp@gmail.com>
2017-04-11 21:16:03 -0700

    1. Created database table item_post_photo 2. Created ItemPostPhoto entity 3. Created associations between ItemPostPhoto and ItemPost 4. Updated logic to create photos 5. Updated the display photo list in views/itempost/show.html.twig

---
+++ src/AppBundle/Entity/ItemPostPhoto.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mayara
+ * Date: 4/11/17
+ * Time: 7:26 PM
+ */
+
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="item_post_photo")
+ * @ORM\Entity()
+ */
+class ItemPostPhoto {
+
+
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="path", type="text")
+     */
+    private $path;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="ItemPost", inversedBy="photo")
+     * @ORM\JoinColumn(name="item_post_id", referencedColumnName="id", nullable=false)
+     */
+    private $itemPost;
+
+    /**
+     * ItemPostPhoto constructor.
+     * path to photo, item post object
+     * @param $path String
+     * @param $itemPost ItemPost
+     */
+    public function __construct($path, $itemPost)
+    {
+        $this->path = $path;
+        $this->itemPost = $itemPost;
+    }
+
+    /**
+     * photo id
+     * @return Integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return String
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * photo's item post
+     * @return ItemPost
+     */
+    public function getItemPost()
+    {
+        return $this->itemPost;
+    }
+
+}
